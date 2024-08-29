0b492a6a38543d718cb999a4627de6edbe9499de
Deepak <deepaktomson@gmail.com>
2024-08-29 11:05:08 +0530

    Updated litnode object instantiation

--- utils/litnode.js
+++ utils/litnode.js
@@ -1,12 +1,14 @@
 import * as LitJsSdk from "@lit-protocol/lit-node-client";
+import { LocalStorage } from "node-localstorage";
 
 // Returns a configured Lit node object
 export const getLitNodeClient = async () => {
-	
+	// Initialize LitNodeClient
 	const litNodeClient = new LitJsSdk.LitNodeClientNodeJs({
 		alertWhenUnauthorized: true,
 		litNetwork: "datil-dev", 
 		debug: true, 
+		storageProvider: new LocalStorage("./lit_storage.db"),
 	});
 	await litNodeClient.connect();
 	return litNodeClient;
